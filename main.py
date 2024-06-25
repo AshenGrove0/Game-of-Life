4564f63fc3a8f55e2daa87a58a3146a441c6fca2
AshenGrove0 <108214445+AshenGrove0@users.noreply.github.com>
2024-06-25 07:26:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pprint
 import time
-
+# work through this in vscode with a proper debugger
 '''
 Any live cell with fewer than two live neighbours dies, as if by underpopulation.
 
@@ -17,11 +17,11 @@ board_diamensions = {
     'x': 5,
     'y': 7
 }
-alive_coords_to_start = [(3,2),(2,1)]
+alive_coords_to_start = [(3,2),(3,3),(3,4)]
 def main():
     board = generate_starting_board()
     pprint.pp(board)
-    run(board,0,10) # let user choose num2
+    run(board) # let user choose num2
     #do this recursively
 
 def generate_starting_board():
@@ -44,10 +44,10 @@ def check_if_all_dead(board) -> bool:
                 alive = True
     return alive
     
-def run(board: list, num_recursions_so_far,num_recursions_wanted):
+def run(board: list):
     ''' FIX ALGORITHM AS CHECKING FOR DEAD CELL DEOSNT WORK PERFECTLY AND TIMESLEEP DOESNT WORK AFTER FIRST RECURSION'''
-    time.sleep(3)
-    num_recursions_so_far += 1
+    time.sleep(2)
+
     #print(board)
     board = list(board)
     all_cell_neighbour_counts = {
@@ -80,12 +80,14 @@ def run(board: list, num_recursions_so_far,num_recursions_wanted):
             except:
                 pass
         all_cell_neighbour_counts.update({(cell,count):neighbour_count})
+    print(all_cell_neighbour_counts)
     #print(all_cell_neighbour_counts)
 
     new_board = board
     for row in range(board_diamensions['y']):
         for column in range(board_diamensions['x']):
             neighbour_count = all_cell_neighbour_counts.get((row, column))
+            
             # good spot to include some tests
             #for live cells
             if board[row][column] == 1:
@@ -98,14 +100,19 @@ def run(board: list, num_recursions_so_far,num_recursions_wanted):
                 if neighbour_count == 3:
                     new_board[row][column] = 1
     
-
+    # check if 0-index errors
     pprint.pp(new_board)
-    all_dead: bool = check_if_all_dead(new_board)
-    if not all_dead:
-        run(new_board,num_recursions_so_far, num_recursions_wanted)
+    #all_dead: bool = check_if_all_dead(new_board)
+    #if all_dead:
+    #    pprint.pp([[0 for col in range(board_diamensions['x'])] for row in range(board_diamensions['y'])])
+    #    print("All dead")
+    #    quit()
+
+    print('new board')
+    print(new_board)
+    print('foothillsofthehimalayas')
+    run(new_board)
         
-    pprint.pp([[0 for col in range(board_diamensions['x'])] for row in range(board_diamensions['y'])])
-    print("All dead")
 
 
     # make it stop if all cells dead
